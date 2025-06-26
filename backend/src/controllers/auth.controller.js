c2f22f6886b9ed7e3e1047aa2d4d6da27371f0a0
francofil <francofil03@gmail.com>
2025-06-26 18:28:50 -0300

    Arranco endpoints auth

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,53 +1,29 @@
 const pool = require('../db/db');
 
-// GET all
-exports.getAllPresidentes = async (req, res) => {
-  const [rows] = await pool.query('SELECT * FROM Users WHERE tipo = "Presidente"');
-  res.json(rows);
-};
-
-// GET one
-exports.getOneCI = async (req, res) => {
-  const [rows] = await pool.query('SELECT * FROM Users WHERE ci = ?', [req.params.ci]);
-  rows.length ? res.json(rows[0]) : res.status(404).json({ error: 'No se encontro presidente con esa cedula de identidad' });
-};
-
-exports.getOneCC = async (req, res) => {
-  const [rows] = await pool.query('SELECT * FROM Users WHERE credencial = ?', [req.params.credencial]);
-  rows.length ? res.json(rows[0]) : res.status(404).json({ error: 'No se encontro presidente con esa credencial' });
-};
 // POST
-/*
-{
-  "ci": "12345678",
-  "credencial": "credencial123",
-  "nombre": "Juan",
-  "apellido": "Pérez",
-  "fecha_nacimiento": "2000-04-15"
-}
- */
 exports.create = async (req, res) => {
-  const { ci, credencial, nombre, apellido, tipo } = req.body;
-  await pool.query(
-    'INSERT INTO Users (ci, credencial, nombre, apellido, tipo) VALUES (?, ?, ?, ?, "Presidente")',
-    [ci, credencial, nombre, apellido, tipo]
-  );
-  res.status(201).json({ message: 'Presidente creado' });
-};
+  const {
+    ci,
+    username,
+    password,
+    rol
+  } = req.body;
 
+  if (rol !== 'admin' && rol !== 'presidente') {
+    return res.status(400).json({ error: 'Rol inválido. Debe ser "admin" o "presidente"' });
+  }
 
-// PUT
-exports.update = async (req, res) => {
-  const { nombre, apellido } = req.body;
-  await pool.query(
-    'UPDATE Users SET nombre = ?, apellido = ? WHERE ci = ?',
-    [nombre, apellido, req.params.ci]
-  );
-  res.json({ message: 'Presidente actualizado' });
-};
+try {
+    const hashed = await bcrypt.hash(password, 10);
+    const [result] = await pool.query(
+      `INSERT INTO Usuario 
+      (ci, username, password, rol)
+      VALUES (?, ?, ?, ?)`,
+      [ci, username, hashed, rol]
+    );
 
-// DELETE
-exports.remove = async (req, res) => {
-  await pool.query('DELETE FROM Users WHERE ci = ?', [req.params.ci]);
-  res.json({ message: 'Presidente eliminado' });
+    res.status(201).json({ message: 'Circuito creado', id: result.insertId });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
