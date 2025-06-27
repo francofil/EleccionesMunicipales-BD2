aa42e153d2725ac6934da2ceda83676d5492519c
francofil <francofil03@gmail.com>
2025-06-27 14:55:25 -0300

    Cambios auth

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 const pool = require('../db/db');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 // POST
 exports.create = async (req, res) => {
@@ -18,8 +20,45 @@ exports.create = async (req, res) => {
       [ci, username, hashed, rol]
     );
 
-    res.status(201).json({ message: 'Circuito creado', id: result.insertId });
+    const token = jwt.sign(
+      { ci: user.ci, rol: user.rol },
+      process.env.JWT_SECRET,
+      { expiresIn: '2h' }
+    );
+    res.status(201).json({ message: 'Circuito creado', toke, });
+  } catch (error) {
+    res.status(500).json({ error: error.message }); 
+  }
+};
+
+//POST
+exports.create = async (req, res) => {
+  const {
+    username,
+    password
+  } = req.body;
+
+  try {
+    const [contraseña] = await pool.query(`SELECT contraseña FROM Usuario WHERE username = ?`,
+      [username]
+    );
+
+    if (contraseña.length === 0) {
+      return res.status(404).json({ error: 'Usuario no encontrado' });
+    }
+
+    valido = await bcrypt.compare(password, contraseña[0]);
+
+    if (!valido) {
+      return res.status(401).json({ error: 'Contraseña incorrecta' });
+    } else {
+
+    }
+
+
+    res.status(201).json({ message: 'Login exitoso', id: result.insertId });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
+
